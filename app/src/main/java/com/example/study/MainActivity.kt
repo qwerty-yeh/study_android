e91702071bd518da861a7d06135857fb42db3ed6
LinkPubAdmin <admin@linkpub.net>
2023-08-28 15:20:44 +0800

    報表欄位測試.

--- app/src/main/java/com/example/study/MainActivity.kt
+++ app/src/main/java/com/example/study/MainActivity.kt
@@ -4,80 +4,171 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.em
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HeaderPreview()
+            Test()
         }
     }
 }
 
-/*
-	<thead>
-	<tr style="width: 100%; height: 50px; margin: 0px; padding: 0px; background-color: #DCDCDC;">
-		<td style="margin: 0px; padding: 0px; min-width: 50px; text-align: center;">
-			<label style="margin: 0px; padding: 0px; color: #575757; font-size: 11pt;" lang_key="no">編號</label>
-		</td>
-		<td style="margin: 0px; padding: 0px; min-width: 100px; text-align: center;">
-			<label style="margin: 0px; padding: 0px; color: #575757; font-size: 11pt;" lang_key="name">名稱</label>
-		</td>
-		<td style="margin: 0px; padding: 0px; width: inherit; text-align: left;">
-			<label style="margin: 0px; padding: 0px 0px 0px 30px; color: #575757; font-size: 11pt;" lang_key="name"></label>
-		</td>
-		<td style="margin: 0px; padding: 0px; min-width: 100px; text-align: center;">
-			<label style="margin: 0px; padding: 0px 0px 0px 30px; color: #575757; font-size: 11pt;" lang_key="count">數量</label>
-		</td>
-	</tr>
-	</thead>
- */
 @Composable
-fun HeaderField(name: String, modifier: Modifier = Modifier) {
-    Text(
+fun NumberHeaderField(name: String, modifier: Modifier = Modifier) {
+    Text(modifier = Modifier
+        .padding(5.dp)
+        .width(100.dp),
         color = Color(0x57, 0x57, 0x57),
-        fontSize = 11.sp,
+        fontSize = 11.em,
+        textAlign = TextAlign.Center,
         text = "$name",
-        modifier = modifier.background(Color(0xDC, 0xDc, 0xDC)).padding(10.dp)
     )
 }
 
 @Composable
-fun Header(names: List<String>, modifier: Modifier = Modifier) {
-    Row(
-        modifier.padding(0.dp).height(33.dp)
+fun StringHeaderField(name: String, modifier: Modifier = Modifier) {
+    Text(modifier = Modifier
+        .padding(5.dp),
+        color = Color(0x57, 0x57, 0x57),
+        fontSize = 11.em,
+        textAlign = TextAlign.Left,
+        text = "$name",
+    )
+}
 
-        // modifier = modifier.background(Color(0xDC, 0xDc, 0xDC))
+@Composable
+fun Header(modifier: Modifier = Modifier) {
+    Row(modifier = Modifier
+        .background(Color(0xDC, 0xDc, 0xDC))
     ) {
-        names.forEach {
-            HeaderField(it)
+        NumberHeaderField("編號")
+        Spacer(modifier = Modifier.width(8.dp))
+        StringHeaderField("名稱")
+        Spacer(modifier = Modifier.width(8.dp))
+        NumberHeaderField("數量")
+    }
+}
+
+@Composable
+fun Test(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = Modifier.background(Color(0xDC,0xDC,0xDC))
+    ) {
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .width(40.dp),
+                    color = Color(0x57,0x57,0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Center,
+                    text = "編號",
+                )
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .weight(1f),
+                    color = Color(0x57, 0x57, 0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Start,
+                    text = "起",
+                )
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .weight(1f),
+                    color = Color(0x57, 0x57, 0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Start,
+                    text = "迄",
+                )
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .width(50.dp),
+                    color = Color(0x57,0x57,0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Right,
+                    text = "時間",
+                )
+            }
+        }
+        item {
+            Row(
+                modifier = Modifier.fillMaxHeight()
+                    .background(Color(0xFF, 0xFF, 0xFF))
+            ) {
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .width(40.dp),
+                    color = Color(0x57, 0x57, 0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Center,
+                    text = "1",
+                )
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .weight(1f, true),
+                    color = Color(0x57, 0x57, 0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Left,
+                    text = "2023-07-30 12:34:56",
+                )
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .weight(1f, true),
+                    color = Color(0x57, 0x57, 0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Left,
+                    text = "2023-07-30 12:34:56",
+                )
+                Text(
+                    modifier = Modifier.padding(5.dp)
+                        .width(50.dp),
+                    color = Color(0x57, 0x57, 0x57),
+                    fontSize = 3.em,
+                    textAlign = TextAlign.Right,
+                    text = "100",
+                )
+            }
         }
     }
-
 }
 
 @Preview()
 @Composable
 fun HeaderFieldPreview() {
-    HeaderField("編號")
+    // NumberHeaderField("編號")
 }
 
 @Preview()
 @Composable
 fun HeaderPreview() {
-    Header(listOf("編號", "名稱", "數量"))
+    // Header()
+}
+
+@Preview()
+@Composable
+fun TestPreview() {
+    Test()
 }
